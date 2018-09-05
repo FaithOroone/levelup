b207da62309193242f42257b906d769443d2a5aa
faithoroone <faithorone59@gmail.com>
2018-09-05 20:53:07 +0300

    fixing errors

--- reception/reception.py
+++ reception/reception.py
@@ -2,38 +2,19 @@ def registration_checker(name):
     file1=open('names/vip_list.txt', 'r')
     content_list1=file1.readlines()
     file1.close()
-
-    file2=open('name/ordonary_list.txt', 'r')
-    content_list1=file1.readlines()
-    file1.close()
     for test_name in content_list1:
         if name.lower() in test_name.lower():
             return(test_name.strip('\n')+',vip ')
 
-    for test_name in content_list1:
+    file2=open('names/ordinary_list.txt', 'r')
+    content_list2=file2.readlines()
+    file2.close()
+
+    for test_name in content_list2:
         if name.lower() in test_name.lower():
             return(test_name.strip('\n')+',ordinary')
     return 'Not Registered'
 
-    print(registration_checker(input('Enter name:')))
 
-
-
-
-    from flask import Flask, Request, jsonify, json
-import sys
-
-sys.path.append("/usr/local/bin/flask")
-
-app = Flask(__name__)
-@app.route('/levelup/learners',methods=['GET'])
-def get_learners():
-    learners_list = {'keren':[{'age':2, 'sex':'female', 'class':'baby'}], 'faith':[{'age':24, 'sex':'female', 'class':'s4'}],'jacob':[{'age':30, 'sex':'male', 'class':'s6'}]}
-    if len(learners_list)==0:
-        return ({"message":"no students"}), 400
-        return jsonify({"learners":learners_list, "number":len(learners_list)}), 200
-
-
-
-if __name__=='__main__':
-    app.run(debug = True, port=5000)
+name=input('Enter name:')
+print(registration_checker(name))
